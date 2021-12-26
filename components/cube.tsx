6beaa74f6e5fdf30915d848c87a5e179ef120021
biohuns <18520474+biohuns@users.noreply.github.com>
2021-12-26 14:57:52 +0900

    refactor: cube.tsx

--- components/cube.tsx
+++ components/cube.tsx
@@ -1,18 +1,18 @@
 import { useCallback, useEffect, useRef } from "react";
 import { cubeSVG, ICubeOptions } from "sr-visualizer";
+import styles from "../styles/components/Cube.module.css";
 
 type Props = {} | ICubeOptions;
 
 export default function Cube(props: Props) {
   const cubeRef = useRef<HTMLDivElement>();
 
-  const clearCube = useCallback(
-    () =>
-      Array.from(cubeRef.current.children).forEach((child) =>
-        cubeRef.current.removeChild(child)
-      ),
-    []
-  );
+  const clearCube = useCallback(() => {
+    if (!cubeRef.current.children) return;
+    Array.from(cubeRef.current.children).forEach((child) =>
+      cubeRef.current.removeChild(child)
+    );
+  }, []);
 
   useEffect(() => {
     const { width, height } = cubeRef.current.getBoundingClientRect();
@@ -24,15 +24,5 @@ export default function Cube(props: Props) {
     return clearCube;
   }, [props, clearCube]);
 
-  return (
-    <>
-      <div ref={cubeRef}></div>
-      <style jsx>{`
-        div {
-          width: 100%;
-          height: 300px;
-        }
-      `}</style>
-    </>
-  );
+  return <div ref={cubeRef} className={styles.cube}></div>;
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function CustomDocument() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={"crossorigin"}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main></Main>
+        <NextScript></NextScript>
+      </body>
+    </Html>
+  );
+}
