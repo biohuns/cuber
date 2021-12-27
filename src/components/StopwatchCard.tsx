edb2da93ff86e3c040d353aea6ddfc309a695bc8
biohuns <18520474+biohuns@users.noreply.github.com>
2021-12-27 22:55:24 +0900

    add: stopwatch limit

--- src/components/StopwatchCard.tsx
+++ src/components/StopwatchCard.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import useStopwatch from "../hooks/stopwatch";
 import Button from "./Button";
 import Card from "./Card";
@@ -5,13 +6,15 @@ import Card from "./Card";
 type Props = {};
 
 export default function StopwatchCard({}: Props) {
-  const { isRunning, ms, start, pause, reset } = useStopwatch({});
+  const { isRunning, ms, start, pause, reset } = useStopwatch({
+    limit: 3600000,
+  });
+  const durationFormat = useDurationFormat();
+  const formatted = durationFormat(ms);
 
   return (
     <Card title="Stopwatch">
-      <div className="time">
-        <span>{(ms / 1000).toFixed(2)}</span>
-      </div>
+      <div className="time">{formatted}</div>
       <div className="buttons">
         <Button onClick={start} disabled={isRunning} color="green">
           Start
@@ -36,4 +39,15 @@ export default function StopwatchCard({}: Props) {
       `}</style>
     </Card>
   );
+}
+
+function useDurationFormat() {
+  return useCallback((ms: number) => {
+    const s = ms / 1000;
+    const sec = (s % 60).toFixed(2).padStart(5, "0");
+    const min = Math.floor(s / 60)
+      .toString()
+      .padStart(2, "0");
+    return `${min}:${sec}`;
+  }, []);
 }
--- src/hooks/stopwatch.ts
+++ src/hooks/stopwatch.ts
@@ -3,17 +3,13 @@ import useInterval from "./interval";
 
 type Props = {
   interval?: number;
+  limit?: number;
 };
 
-export default function useStopwatch({ interval = 10 }: Props) {
+export default function useStopwatch({ interval = 10, limit }: Props) {
   const [isRunning, setIsRunning] = useState(false);
   const [startMs, setStartMs] = useState(NaN);
   const [ms, setMs] = useState(0);
-
-  useInterval(
-    () => setMs(Number.isNaN(startMs) ? 0 : getNowMs() - startMs),
-    isRunning ? interval : undefined
-  );
 
   const start = () => {
     setStartMs(getNowMs() - ms);
@@ -29,6 +25,14 @@ export default function useStopwatch({ interval = 10 }: Props) {
     setStartMs(NaN);
     setMs(0);
   };
+
+  useInterval(
+    () => {
+      setMs(Number.isNaN(startMs) ? 0 : getNowMs() - startMs);
+      if (limit !== undefined && limit < ms) reset();
+    },
+    isRunning ? interval : undefined
+  );
 
   return { isRunning, ms, start, pause, reset };
 }
