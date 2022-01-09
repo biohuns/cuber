38bdf87aef81a3f97ba893bfaac3e8440620d712
biohuns <18520474+biohuns@users.noreply.github.com>
2022-01-09 20:07:09 +0900

    delete viewport setting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,10 +10,6 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-        />
         <meta name="application-name" content={appName} />
         {/* <meta name='description' content='Best PWA App in the world' /> */}
 
